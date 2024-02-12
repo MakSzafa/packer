2c29721d7ee468e7111601041db3998a254dcca2
Max Szafarski <makszafa@gmail.com>
2024-02-12 13:12:49 +0100

    init nuxt vuetify project

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  css: ['~/assets/main.scss'],
+  devtools: { enabled: true }, // devtools on website
+  app: {
+    head: {
+      title: 'Packer',
+      meta: [
+        { name: 'description', content: 'With this app packing will be easier'}
+      ]
+    }
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
