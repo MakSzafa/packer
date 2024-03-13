93cb9ec97505bdd515724ad01fe1970928aefaa4
Max Szafarski <makszafa@gmail.com>
2024-03-13 14:33:17 +0100

    init header and footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-footer class="bg-header text-on-header flex-0-1 justify-center">
+        <v-btn class="mx-3" rounded="xl" variant="text" @click="navigateTo('/about')">
+            Informacje
+        </v-btn>
+        <v-btn class="mx-3" rounded="xl" variant="text" @click="navigateTo('https://buycoffee.to/maniek_programista', {open:{target: '_blank'}})">
+            Wesprzyj mnie
+        </v-btn>
+        <v-btn class="mx-3" rounded="xl" variant="text" @click="navigateTo('https://makszafa.pl', {open:{target: '_blank'}})">
+            Kontakt
+        </v-btn>
+    </v-footer>
+</template>
+
---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar scroll-behavior="elevate" scroll-threshold="250" density="compact" class="bg-header text-on-header">
+        <v-app-bar-title class="d-flex justify-center font-weight-bold text-h4 cursor-pointer" @click="navigateTo('/')">Packer</v-app-bar-title>
+        <v-btn icon="mdi-theme-light-dark" @click="toggleTheme"></v-btn>
+    </v-app-bar>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+    theme.global.name.value = theme.global.current.value.dark ? 'customLightTheme' : 'customDarkTheme'
+}
+</script>
