29cec03cc8d909524b026e09b2497ef5531b3e8a
Max Szafarski <makszafa@gmail.com>
2024-03-15 13:38:48 +0100

    create chip group card

---
+++ components/ChipGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-card class="ma-2" :title="title">
+            <v-chip-group 
+            column 
+            multiple 
+            color="primary"
+            v-model="pickedChips" 
+            @update:model-value="emitChipGroupValue">
+                <v-chip 
+                v-for="chip in chips" 
+                :key="chip.id"  
+                :value="chip.name"
+                class="mx-3" >
+                {{ chip.name }}</v-chip>
+
+            </v-chip-group>
+    </v-card>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    chips: {
+        type: Array,
+        required: true,
+    },
+})
+
+const pickedChips = ref([]);
+
+const emit = defineEmits(['chipGroupValueUpdated'])
+
+const emitChipGroupValue = () => {
+    emit('chipGroupValueUpdated', pickedChips.value)
+}
+</script>
